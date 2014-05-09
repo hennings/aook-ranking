09c49d68644dd36606b407693400c980c98c46d0
Henning Spjelkavik <henning@spjelkavik.net>
2014-05-09 13:09:02 +0200

    First race 2014.

---
+++ 2013/run_import.sh
@@ -0,0 +1,38 @@
+perl import.pl data/lk-2013.txt 4 D13-14 "D 13-14" 2648
+perl import.pl data/lk-2013.txt 4 H13-14 "H 13-14" 2648
+perl import.pl data/lk-2013.txt 4 D15-16 "D 15-16" 2648
+perl import.pl data/lk-2013.txt 4 H15-16 "H 15-16" 2648
+
+perl import.pl data/nydalten-2013.xml 5 D13-14 "D 13-14"
+perl import.pl data/nydalten-2013.xml 5 H13-14 "H 13-14"
+perl import.pl data/nydalten-2013.xml 5 D15-16 "D 15-16"
+perl import.pl data/nydalten-2013.xml 5 H15-16 "H 15-16"
+
+perl import.pl data/follolopet-2013.xml 6 D13-14 "D 13-14"
+perl import.pl data/follolopet-2013.xml 6 H13-14 "H 13-14"
+perl import.pl data/follolopet-2013.xml 6 D15-16 "D 15-16"
+perl import.pl data/follolopet-2013.xml 6 H15-16 "H 15-16"
+
+
+perl calc.pl 4 D13-14
+perl calc.pl 4 H13-14
+perl calc.pl 4 D15-16
+perl calc.pl 4 H15-16
+
+perl calc.pl 5 D13-14
+perl calc.pl 5 H13-14
+perl calc.pl 5 D15-16
+perl calc.pl 5 H15-16
+
+perl calc.pl 6 D13-14
+perl calc.pl 6 H13-14
+perl calc.pl 6 D15-16
+perl calc.pl 6 H15-16
+
+#perl calc.pl 2 D13-14
+#perl calc.pl 2 H13-14
+#perl calc.pl 2 D15-16
+#perl calc.pl 2 H15-16
+
+perl present.pl 2
+ 
